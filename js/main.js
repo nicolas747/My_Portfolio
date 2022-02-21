e00dd0759c12523920b487c73eee36e89a27f718
nicolas747 <nmarquez9831@gmail.com>
2022-02-21 16:27:47 -0500

    My Portafolio Actualizado

--- js/main.js
+++ js/main.js
@@ -1,17 +1,85 @@
-/*Sobre Mi Seccion*/
-(() =>{
-    const aboutSection = document.querySelector(".about-section"), tabsContainer = document.querySelector(".about-tabs");
 
-    tabsContainer.addEventListener("click", (even) =>{
-        if(event.target.classList.contains("tab-items") && !event.target.classList.contains ("active")){
-            const target = event.target.getAttribute("data-target");
-            
-            tabsContainer.querySelector(".active").classList.remove("outer-shadow", "active");
+ /*---------menu de navegacion----------*/
+ (() =>{
+     const hamburgerBtn = document.querySelector(".hamburger-btn"),
+     navMenu = document.querySelector(".nav-menu"),
+     closeNavBtn = navMenu.querySelector(".close-nav-menu");
 
-            event.target.classList.add("active", "outer-shadow");
+     hamburgerBtn.addEventListener("click", showNavMenu);
+     closeNavBtn.addEventListener("click", hideNavMenu);
 
-            aboutSection.querySelector(".tab-content.active").classList.add("active");
-            }
-        
-    })
-})();
+     function showNavMenu(){
+         navMenu.classList.add("open");
+     }
+     function hideNavMenu(){
+         navMenu.classList.remove("open");
+         fadeOutEffect();
+     }
+     function fadeOutEffect(){
+         document.querySelector(".fade-out-effect").classList.add("active");
+         setTimeout(() =>{
+            document.querySelector(".fade-out-effect").classList.remove("active");
+
+         },300)
+     }
+
+     document.addEventListener("click", (event) =>{
+          if(event.target.classList.contains('link-item')){
+
+              if(event.target.hash !==""){
+
+                  event.preventDefault();
+                  const hash = event.target.hash;
+
+                  document.querySelector(".section.active").classList.add("hide");
+                  document.querySelector(".section.active").classList.remove("active");
+
+                  document.querySelector(hash).classList.add("active");
+                  document.querySelector(hash).classList.remove("hide");
+
+                  if(navMenu.classList.contains("open")){
+                     event.target.classList.add("active","inner-shadow");
+                     event.target.classList.remove("outer-shadow","hover-in-shadow");
+                     hideNavMenu();
+                  }
+                  else{
+                      let navItems = navMenu.querySelectorAll(".link-item");
+                      navItems.forEach((item) =>{
+                          if(hash === item.hash){
+
+                          }
+                      })
+                      fadeOutEffect();
+                  }
+                 //  ver url
+                 window.location.hash = hash;
+
+              }
+          }
+
+     })
+
+ })();
+
+function bodyScrollingToggle(){
+    document.body.classList.toggle("stop-scrolling");
+}
+
+
+// /*---------Secciones ocultas excepto cuando esta activa--------*/
+
+ (() =>{
+     const sections = document.querySelectorAll(".section");
+     sections.forEach((section) =>{
+         if(!section.classList.contains("active")){
+             section.classList.add("hide");
+         }
+     })
+ })();
+
+ window.addEventListener("load", () =>{
+     document.querySelector(".preloader").classList.add("fade-out");
+     setTimeout(() => {
+         document.querySelector(".preloader").style.display = "none";
+     },600)
+ })
